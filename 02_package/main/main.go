411233dda16d400e10e0c0dd418fa3b5aee011da
robertelving <vivalaklask@gmail.com>
2015-12-05 12:25:30 +0100

    part 2

---
+++ 02_package/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/robertelving/udemyTraining/02_package/stringutil"
+
+func main()  {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
